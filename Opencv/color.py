ff8dcccef65262419e9729dd15f57ee39565ddcf
Ni-ChiTing <dfdf22222@gmail.com>
2018-07-26 18:00:59 +0800

    add mask picture

--- Opencv/color.py
+++ Opencv/color.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-
-img = cv2.imread('/home/user/cam1.jpg')
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_blue = np.array([98,43,46])
-upper_blue = np.array([102,255,255])
-mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-res_blue = cv2.bitwise_and(img,img, mask= mask_blue)
-cv2.imwrite('save.jpg',res_blue)
+while True:
+	inimg=raw_input("name:")
+	img = cv2.imread("/home/user/git/UAV/img/"+inimg)
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([96,43,46])
+	upper_blue = np.array([113,255,255])
+	mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+	res_blue = cv2.bitwise_and(img,img, mask= mask_blue)
+	cv2.imwrite('mask_'+inimg+'.jpg',res_blue)
