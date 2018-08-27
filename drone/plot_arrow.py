1dd87d894e8e18669e3e1e2e264791e90681181e
siahuat0727 <tansiahuat@gmail.com>
2018-08-27 17:34:33 +0800

    update

--- drone/plot_arrow.py
+++ drone/plot_arrow.py
@@ -5,18 +5,21 @@ from __future__ import print_function
 
 from os import listdir
 from os.path import isfile, join, dirname
+import cv2
 
 import sys
 sys.path.append(join(dirname(__file__), '..', 'Opencv'))
 from lib import imread, imwrite, rgb2blue, detect_circles, alt2diagonal, best_circle, draw_circle, plot_arrow
 
 def haha(name, kk):
-  import cv2
   cv2.waitKey(0)
 
 from picamera import PiCamera
+import time
 camera = PiCamera()
-
+camera.resolution=(150,150)
+cv2.namedWindow('haha',cv2.WINDOW_NORMAL)
+img_path = 'test.jpg'
 first = True
 while True:
   camera.capture(img_path)
@@ -24,15 +27,16 @@ while True:
   # detect circles
   img_ = rgb2blue(img)
   circles = detect_circles(img_, 0)
-  circle = best_circle(circles, img_)
-  img = plot_arrow(img, (circle[0], circle[1]))
+  circle = best_circle(circles, img_, camera.resolution)
+  if circle is not None:
+    img = plot_arrow(img, (circle[0], circle[1]), center=(camera.resolution[0]//2, camera.resolution[1]//2))
+    img = add_text(img, H=100, D=80, Min=0, Max=100):
+  cv2.startWindowThread()
   cv2.imshow('haha', img)
-  if first:
-    first = False
-    import thread
-    thread.start_new_thread(haha, (1,2))
+cv2.destroyAllWindows()
 
-import time
-while True:
-  time.sleep(1)
-  print('haha')
+  # cv2.waitKey(0)
+  # if first:
+  #   first = False
+  #   import thread
+  #   thread.start_new_thread(haha, (1,2))
