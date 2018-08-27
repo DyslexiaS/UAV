b9949de9f5cacca23aa790189d5a74619489a843
DyslexiaS <exist02593@gmail.com>
2018-08-27 14:24:06 +0800

    add text to lib.

--- Opencv/lib.py
+++ Opencv/lib.py
@@ -97,7 +97,7 @@ def draw_circle(img, circle):
   cv2.circle(img, center, radius, (255,255,255),3, 8, 0)
   return img
 
-def plot_arrow(img, point, center=(360,240)):
+def plot_arrow(img, point, center=(75,75)):
   cv2.line(img, center, point, (0,0,255), 5)
   pi = 3.1415926
   angle = math.atan2(center[1]-point[1], center[0]-point[0])
@@ -107,6 +107,20 @@ def plot_arrow(img, point, center=(360,240)):
   arrow_x = point[0] + 20* math.cos(angle-pi*30/180)
   arrow_y = point[1] + 20* math.sin(angle-pi*30/180)
   cv2.line(img, point, (int(arrow_x),int(arrow_y)), (0,0,255), 5)
+  return img
+
+def add_text(img, H=100, D=80, Min=0, Max=100):
+  if D>Max:
+      color = (0,0,255)
+  elif D<Min:
+      color = (0,255,0)
+  else:
+      color = (0,255*(Max-D)/(Max-Min),255-255*(Max-D)/(Max-Min))
+
+  text = 'Height: '+ str(H)
+  cv2.putText(img,text,(75,120),cv2.FONT_HERSHEY_DUPLEX,0.3,color,1)
+  text = 'Distance: '+ str(D)
+  cv2.putText(img,text,(75,130),cv2.FONT_HERSHEY_DUPLEX,0.3,color,1)
   return img
 
   # if circles is not None  :
